5ea6f36ee6b294731804c192ebd20fa663afe234
Frazer McLean <frazergmclean@gmail.com>
2013-11-17 02:43:30 +0000

    Add rad2deg and deg2rad

---
+++ rad2deg.m
@@ -0,0 +1,15 @@
+function deg = rad2deg(rad)
+    % RAD2DEG Convert radians to degrees.
+    %
+    %     RAD2DEG(rad) converts a scalar, vector or matrix of radians into
+    %     degrees.
+    %
+    %     Examples
+    %         RAD2DEG(pi)                     % 180
+    %         RAD2DEG([pi/2 pi 6*pi/4 2*pi])  % [90 180 270 360]
+    %
+    %     See also deg2rad.
+    %
+    % Frazer McLean <frazergmclean@gmail.com>
+	deg = rad*180/pi;
+end
